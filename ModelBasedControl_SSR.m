01507e82f03224e29bad40f1fb40786afad95d48
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2021-05-04 20:31:29 +0530

    Tried out code for simulating the model based control system for underactuated plant (horizontal actuation of cart and no actuation of pole) - unstable control system at all times

--- ModelBasedControl_SSR.m
+++ ModelBasedControl_SSR.m
@@ -1,6 +1,6 @@
-function [dw_dt] = ModelBasedControl_SSR(t,w,M_mat,K_mat,Kc,A,B)
+function [dw_dt] = ModelBasedControl_SSR(t,w,M_mat,K_mat,Kc,Kp,Kd,A,B,controller,act)
 
-% State Space Model fora Model Based Control System Closed Loop
+% State Space Model for a Model Based Control System Closed Loop
 % w: State Vector
 % dw_dt: First Time Differential of State Vector
 % X = [w1;w2];
@@ -14,8 +14,23 @@ function [dw_dt] = ModelBasedControl_SSR(t,w,M_mat,K_mat,Kc,A,B)
 % u = -M_mat*Kc*[w(1);w(2)] + K_mat*[w(1);w(2)];
 % dw_dt = A*w + B*u;
 
+% act: variable defining the type of actuation in the system
+% controller: variable defining the kind of controller used in the system
+
 % Closed Loop State Weighing Matrix
-A_cl = A + [B*(K_mat - M_mat*Kc), zeros(4,2)];
-% A_cl = A + [B*(K_mat - M_mat*Kp), -B*M_mat*Kd];
+switch controller
+    case 'General'
+        A_cl = A + [B*(K_mat - M_mat*Kc), zeros(4,2)];;
+    case 'PD'
+        switch act
+            case 'Both'
+                A_cl = A + [B*(K_mat - M_mat*Kp), -B*M_mat*Kd];
+            case 'Cart'
+                A_cl = A + [B*[1,0]*(K_mat - M_mat*Kp), -B*[1,0]*M_mat*Kd];
+            case 'Pole'
+                A_cl = A + [B*[0,1]*(K_mat - M_mat*Kp), -B*[0,1]*M_mat*Kd];
+        end      
+end
+
 dw_dt = A_cl*w;
 end
